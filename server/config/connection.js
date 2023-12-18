56e2e76e8a8fcc776813a90bdb452a247e63a912
Latrell Page <pagelatrell14@gmail.com>
2023-12-18 11:22:54 -0600

    COnnected to production database

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-	process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/CuriosityAI",
+	process.env.MONGODB_URI || "mongodb+srv://pagelatrell14:CuriosityAI1@curiosityai.zkox2rc.mongodb.net/",
 	{
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
