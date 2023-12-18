efbe6d79beaf403a9b8a05026a0b275e85c49603
Latrell Page <pagelatrell14@gmail.com>
2023-12-18 11:48:47 -0600

    Test

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-	process.env.MONGODB_URI || "mongodb+srv://pagelatrell14:CuriosityAI1@curiosityai.zkox2rc.mongodb.net/",
+	process.env.MONGODB_URI || "mongodb+srv://pagelatrell14:CuriosityAI1@curiosityai.zkox2rc.mongodb.net/CuriosityAI",
 	{
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
