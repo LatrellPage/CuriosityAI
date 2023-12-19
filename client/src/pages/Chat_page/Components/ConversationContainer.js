de21b10458e172ddfcbd70760ebe30e313ca3284
Latrell Page <pagelatrell14@gmail.com>
2023-12-19 03:12:17 -0600

    Changed AI image path

--- client/src/pages/Chat_page/Components/ConversationContainer.js
+++ client/src/pages/Chat_page/Components/ConversationContainer.js
@@ -59,7 +59,7 @@ const Message = ({ role, content }) => {
 					</Avatar>
 				) : (
 					<img
-						src="/robotThinking.jpg" 
+						src="robotThinking.jpg" 
 						alt="AI Avatar"
 						className="profile-avatar"
 					/>
